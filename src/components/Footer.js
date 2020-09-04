52d48387b0c1df08bf002e5cf8847feefc928e10
Jonathan Christie <jchristie203@gmail.com>
2020-09-03 22:49:39 -0400

    Changed logo

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Link } from 'gatsby'
 
-import logo from '../img/logo.svg'
+import logo from '../img/logo.png'
 import facebook from '../img/social/facebook.svg'
 import instagram from '../img/social/instagram.svg'
 import twitter from '../img/social/twitter.svg'
@@ -11,12 +11,10 @@ const Footer = class extends React.Component {
   render() {
     return (
       <footer className="footer footer-background has-text-white-ter">
-        <div className="content has-text-centered">
-          <img
-            src={logo}
-            alt="Kaldi"
-            style={{ width: '14em', height: '10em' }}
-          />
+        <div className="content has-text-centered has-text-bold">
+          <h1 style= {{
+              color: 'rgb(78,115,185)'
+          }}>Joyful Living Reiki</h1>
         </div>
         <div className="content has-text-centered footer-background has-text-white-ter">
           <div className="container footer-background has-text-white-ter">
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Link } from 'gatsby'
 import github from '../img/github-icon.svg'
-import logo from '../img/logo.svg'
+import logo from '../img/logo.png'
 
 const Navbar = class extends React.Component {
   constructor(props) {
@@ -41,8 +41,10 @@ const Navbar = class extends React.Component {
       >
         <div className="container">
           <div className="navbar-brand">
-            <Link to="/" className="navbar-item" title="Logo">
-              <img src={logo} alt="Kaldi" style={{ width: '88px' }} />
+            <Link to="/" className="navbar-item"  title="Logo">
+              <h1 class="has-text-bold" style={{
+                  color: 'rgb(78,115,185)'
+              }}>Joyful Living Reiki</h1>
             </Link>
             {/* Hamburger menu */}
             <div
@@ -64,7 +66,7 @@ const Navbar = class extends React.Component {
                 About
               </Link>
               <Link className="navbar-item" to="/products">
-                Products
+                 Sessions and Classes
               </Link>
               <Link className="navbar-item" to="/blog">
                 Blog
