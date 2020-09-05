e3505317e4dcd329bcab7d69f6cfeb656af01e6e
Jonathan Christie <jchristie203@gmail.com>
2020-09-05 13:30:03 -0400

    changed bulma proportions

--- src/components/Sidebyside.js
+++ src/components/Sidebyside.js
@@ -6,16 +6,14 @@ const FeatureGrid = ({ gridItems }) => (
   <div>
     {gridItems.map((item) => (
       <div key={item.text} className="tile is-ancestor">
-            <div className='tile is-4 vertical is-parent' >
+            <div className='tile is-6 vertical is-parent' >
               <div class="tile is-child box">
                 <PreviewCompatibleImage  imageInfo={item} />
               </div>
             </div>
           <div className="tile is-parent vertical">
-            <div className='tile is-child'>
-              <h2 style={{fontWeight: 'bold'}}>{item.heading}</h2>
-            </div>
             <div className='tile is-child box'>
+              <h2 style={{fontWeight: 'bold'}}>{item.heading}</h2>
               <p class='text'>{item.text}</p>
             </div>
           </div>
