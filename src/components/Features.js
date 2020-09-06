e2ee7a7ce94ad7e29cb54b0c31f5a44972b5846c
Jonathan Christie <jchristie203@gmail.com>
2020-09-06 17:57:50 -0400

    Fixed mobile formatting on Product Page

--- src/components/Features.js
+++ src/components/Features.js
@@ -5,21 +5,18 @@ import PreviewCompatibleImage from '../components/PreviewCompatibleImage'
 const FeatureGrid = ({ gridItems }) => (
   <div className="columns is-multiline">
     {gridItems.map((item) => (
-      <div key={item.text} className="column is-6" style={{padding: 'none'}}>
-        <section className="section">
+      <div key={item.text} className="column is-6">
           <div className="has-text-centered">
             <div
               style={{
-                width: '300px',
-                height: '170px',
-                display: 'inline-block'
+                width: '240px',
+                display: 'inline-block',
               }}
             >
               <PreviewCompatibleImage imageInfo={item} />
             </div>
           </div>
           <p>{item.text}</p>
-        </section>
       </div>
     ))}
   </div>
